e6032a2f2794e022896c16bde4631d519bce5972
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-05-26 15:55:59 -0400

    Change CSS

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="display-1">Pantry Manager</h1>
-        <h2 className="display-3">Welcome to your virtual pantry!</h2>
-        <h4 className="display-4">Here's your current list of products:</h4>
+        <header>
+          <h1 className="display-1">Pantry Manager</h1>
+          <h2 className="display-3">Welcome to your virtual pantry!</h2>
+        </header>
+        <h4 className="display-6">Here's your current list of products:</h4>
+
         <Table />
       </div>
     </div>
