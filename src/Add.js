3aa7c9637da4770a61a774133a2bd9dc25f1f3d6
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-05-26 17:33:01 -0400

    Add products to DB and display them on screen

--- src/Add.js
+++ src/Add.js
@@ -1,13 +1,27 @@
 import React from "react";
+import axios from "axios";
 
 export default function Add() {
+  //
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let newProduct = {
+      product: event.target[0].value,
+      amount: +event.target[1].value,
+    };
+    console.log(newProduct);
+    const apiUrl = `https://60a43063fbd48100179dbb84.mockapi.io/products`;
+    axios.post(apiUrl, newProduct).then();
+  }
+
   return (
     <div className="Add">
       <div className="container">
         <h4 className="display-6">Add a new product:</h4>
 
-        <form className="row g-3">
-          <div className="col-6 offset-1">
+        <form className="row g-3" onSubmit={handleSubmit}>
+          <div className="col-md-6 offset-md-1">
             <span>
               <input
                 type="text"
@@ -16,7 +30,7 @@ export default function Add() {
               />
             </span>
           </div>
-          <div className="col-2">
+          <div className="col-md-2">
             <span>
               <input
                 type="number"
@@ -25,7 +39,7 @@ export default function Add() {
               />
             </span>
           </div>
-          <div className="col-auto">
+          <div className="col-md-auto">
             <button type="submit" className="btn btn-primary mb-3">
               Add product
             </button>
